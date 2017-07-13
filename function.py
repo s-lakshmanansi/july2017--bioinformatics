6e52d150bc10ed6fadf2095218bfd1deaa9d006b
s-lakshmanansi <sidharth.m.lakshmanan@gmail.com>
2017-07-13 11:45:21 -0700

    Update function.py

--- function.py
+++ function.py
@@ -1,1 +1,19 @@
+def palindrome(s):
+
+        t=""
+        for i in range(0,len(s)):
+                if s[i]=="A":
+                        t=t+"T"
+                if s[i]=="T":
+                        t=t+"A"
+                if s[i]=="G":
+                        t=t+"C"
+                if s[i]=="C":
+                        t=t+"G"
+        x=t[::-1]
+        if x==s:
+                return "It is a palindrome"
+        else:
+                return "It is not a palindrome"
+
 
